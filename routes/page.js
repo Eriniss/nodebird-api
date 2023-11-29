692ba4c4c11d92830738e2d7d2bb0cd87d8c46d5
Eriniss <jhs2195@gmail.com>
2023-11-29 22:18:14 +0900

    :sparkles: Post 라우터 추가

--- routes/page.js
+++ routes/page.js
@@ -3,8 +3,29 @@
 
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('../middleware');
+const { Post, User } = require('../models');
 
 const router = express.Router();
+
+// ...
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(err);
+    next(err);
+  }
+});
 
 // 모든 라우터에서 사용되는 미들웨어 설정
 // 아래의 변수들은 모든 템플릿 엔진에서 공통으로 사용됩니다.
